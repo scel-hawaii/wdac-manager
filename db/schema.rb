76f5d794de5bd86b6c75e37912729d8e884fed77
Kenny Luong <luong97@hawaii.edu>
2016-04-06 19:54:49 -1000

    Added description to dataset

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160407054914) do
+ActiveRecord::Schema.define(version: 20160407055259) do
 
   create_table "datasets", force: :cascade do |t|
     t.datetime "start_time"
@@ -19,8 +19,9 @@ ActiveRecord::Schema.define(version: 20160407054914) do
     t.integer  "address"
     t.string   "name"
     t.string   "author"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.text     "description"
   end
 
   create_table "nodes", force: :cascade do |t|
